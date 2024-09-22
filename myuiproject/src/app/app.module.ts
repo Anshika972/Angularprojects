9994a74c522ea4076153a7bc8907dd3467e10fd8
Anshika972 <anshikadubey972@gmail.com>
2024-09-22 14:27:05 +0530

    commit

--- myuiproject/src/app/app.module.ts
+++ myuiproject/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomepageComponent,
-    RegistrationpageComponent,
-    
+    AppComponent
   ],
   imports: [
     BrowserModule,
