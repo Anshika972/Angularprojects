96fecf6821be3d033e59b295dc65514818791771
Anshika972 <anshikadubey972@gmail.com>
2024-09-29 19:05:50 +0530

    commit

--- myuiproject/src/app/app-routing.module.ts
+++ myuiproject/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginpageComponent },
+  { path: 'register', component: RegistrationpageComponent },
+  { path: 'contactus', component: ContactusComponent},
+  { path: 'home', component: HomepageComponent },
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myuiproject/src/app/app.module.ts
+++ myuiproject/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    RegistrationpageComponent,
+    ContactusComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
